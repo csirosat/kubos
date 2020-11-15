9dd60f3485a91c54a14ddecce41f834399788633
Mike Pilawa <Mike.Pilawa@csiro.au>
2020-11-16 09:56:02 +1100

    Copy of target executables moved to mkupdate.sh script in horus project.

--- build_all.sh
+++ build_all.sh
@@ -15,7 +15,6 @@ shell-service
 telemetry-service
 "
 TARGET_DIR="target/thumbv7m-unknown-linux-uclibc/release/"
-DEST_DIR="../../linux-m2s/projects/horus/update/usr/local/sbin/"
 
 for APP in $APPS
 do
@@ -23,6 +22,4 @@ do
     xargo build --target thumbv7m-unknown-linux-uclibc -p $APP --release
   $FLTHDR -s 0x10000 $TARGET_DIR/$APP
 done
-
-echo "$APPS" | rsync -av --files-from=- $TARGET_DIR $DEST_DIR
 
