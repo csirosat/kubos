f58175d3554432caa877b4084bf7aeced09ed674
Mike Pilawa <Mike.Pilawa@csiro.au>
2021-01-06 15:41:02 +1100

    Moved rust/src dependency to be directly under third-party dir. Made build_apps.sh more readable.

--- build_apps.sh
+++ build_apps.sh
@@ -19,7 +19,7 @@ TARGET_DIR="target/thumbv7m-unknown-linux-uclibc/release/"
 for APP in $APPS
 do
   PKG_CONFIG_ALLOW_CROSS=1 RUST_TARGET_PATH=`pwd` \
-    xargo build --target thumbv7m-unknown-linux-uclibc -p $APP --release
+    xargo build --target thumbv7m-unknown-linux-uclibc --release --package $APP
   $FLTHDR -s 0x10000 $TARGET_DIR/$APP
 done
 
--- env.sh
+++ env.sh
@@ -3,12 +3,11 @@
 ROOT=`git rev-parse --show-superproject-working-tree`
 TOOLCHAINS=$ROOT/toolchains
 THIRDPARTY=$ROOT/third-party
-DEPS=$THIRDPARTY/uclinux-port/dependencies
 
 export     CC=$TOOLCHAINS/buildroot/host/bin/arm-buildroot-uclinux-uclibcgnueabi-gcc
 export    CXX=$TOOLCHAINS/buildroot/host/bin/arm-buildroot-uclinux-uclibcgnueabi-g++
 export FLTHDR=$TOOLCHAINS/buildroot/host/bin/arm-buildroot-uclinux-uclibcgnueabi-flthdr
-export XARGO_RUST_SRC=$DEPS/rust/src
+export XARGO_RUST_SRC=$THIRDPARTY/rust/src
 
 echo "Environment Configured Successfully"
 
