25cf63f23f29ddc6ce5a72202151b061a295f562
Mike Pilawa <Mike.Pilawa@csiro.au>
2021-04-12 16:46:40 +1000

    Echo the app we are building.

--- build_apps.sh
+++ build_apps.sh
@@ -17,6 +17,7 @@ TARGET_DIR="target/thumbv7m-unknown-linux-uclibc/release/"
 
 for APP in $APPS
 do
+  echo "Building $APP ..."
   PKG_CONFIG_ALLOW_CROSS=1 RUST_TARGET_PATH=`pwd` \
     xargo build --target thumbv7m-unknown-linux-uclibc --release --package $APP
   $FLTHDR -s 0x10000 $TARGET_DIR/$APP
